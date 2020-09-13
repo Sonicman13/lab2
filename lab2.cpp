1de7f9d3946ea58ef13bb23dc3d01f11d38f717b
Sonicman13 <70899358+Sonicman13@users.noreply.github.com>
2020-09-13 18:59:14 +0300

    changed struct and read

--- lab2.cpp
+++ lab2.cpp
@@ -2,31 +2,44 @@
 //
 
 #include <iostream>
+#include "stdio.h"
+#include "conio.h"
 #define N 100
 
-struct item {
-    char code[N];
+struct store {
     char name[N];
-    double price;
-    int amount;
+    char adress[N];
+    int numberOfItems;
+    struct item {
+        char code[N];
+        char name[N];
+        double price;
+        int amount;
+    }item;
 };
 
-void read(struct item* item1) {
-    char s[N];
-    double d;
-    int i;
-    printf("Введите код товара\n");
-    scanf("%s", &s);
-    strcpy(item1->code, s);
-    printf("Введите название товара\n");
-    scanf("%s", &s);
-    strcpy(item1->name, s);
-    printf("Введите цену\n");
-    scanf("%lf", &d);
-    item1->price = d;
-    printf("Введите колличество товара\n");
-    scanf("%d", &i);
-    item1->amount = i;
+
+void read(struct store* store1) {
+    char f;
+    printf("Введите название магазина\n");
+    scanf("%s", store1->name);
+    printf("Введите адрес магазина\n");
+    scanf("%s", store1->adress);
+    store1->numberOfItems = 0;
+    printf("Добавить товар?(1-да,0-нет)\n");
+    f = getche();
+    while (f == '1') {
+        printf("Введите название товара\n");
+        scanf("%s", store1->item.name);
+        printf("Введите код товара\n");
+        scanf("%s", store1->item.code);
+        printf("Введите цену\n");
+        scanf("%lf", &store1->item.price);
+        printf("Введите колличество товара\n");
+        scanf("%d", &store1->item.amount);
+        printf("Добавить еще один товар?(1-да,0-нет)\n");
+        f = getche();
+    }
 }
 
 int main()
